5902529f4e39041386aec933a911327ef4d6f704
SziNo <szino1848@gmail.com>
2025-02-11 14:17:54 +0100

    configure CORS middleware with correct origin and credentials settings (somehow managed to paste it in express.json)

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
 app.use(
-  express.json({
+  cors({
     origin: "https://questionnaire-frontend-nine.vercel.app",
     credentials: true,
   })
 );
+app.use(express.json());
 
 connectDB();
 
